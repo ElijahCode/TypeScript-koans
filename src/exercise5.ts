dfc1e17322d446ac6c2009b687f3d96a269f5a4b
Ilya <ilyastarickov1996@gmail.com>
2021-05-12 20:34:51 +0400

    feat: change solution for exercise 5

--- src/exercise5.ts
+++ src/exercise5.ts
@@ -2,7 +2,7 @@
 // Нужно заменить FIXME на правильный тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = Partial<React.ComponentType> | undefined;
+type FIXME = React.ComponentType["defaultProps"] | undefined;
 
 // Hint: infer
 export const getDefaultProps = <T>(component: React.ComponentType<T>): FIXME =>
